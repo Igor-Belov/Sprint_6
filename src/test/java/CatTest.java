e866a1bfd94a1d4bdd408380a883f67cab8e58d4
Igor Belov <Belovis@yandex.ru>
2024-09-28 21:25:55 +0300

    Удалил мусор.

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,6 +1,5 @@
 import com.example.Cat;
 import com.example.Feline;
-import com.example.Predator;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +7,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.lang.reflect.Array;
 import java.util.List;
 
 import static org.mockito.Mockito.times;
